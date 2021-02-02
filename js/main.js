564aaf518863ce3395c09239a100647201fed47d
carmenlop <cllopez92@gmail.com>
2021-02-01 19:55:02 -0800

    eod commit monday

--- js/main.js
+++ js/main.js
@@ -5,30 +5,50 @@ console.log('sanity check!')
 
 
 
+let pokemonInput = document.getElementById('pokemon-names')
 
 
-let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/gengar'
 
-fetch(pokeUrl)
-.then((responseData) => {
-    return responseData.json();
-})
-.then((jsonData) => {
-    jsonData.types.forEach((result) => {
-        fetch(result.type.url)
-        .then((typeresonseData) => {
-            return typeresonseData.json();
-        })
-        .then((typejsonData) => {
-            typejsonData.damage_relations.double_damage_from.forEach(doubleDamageFrom => console.log("double damage from " + doubleDamageFrom.name))
-    
-            typejsonData.damage_relations.double_damage_to.forEach(doubleDamageTo => console.log("double damage to " + doubleDamageTo.name))
-    
-            typejsonData.damage_relations.half_damage_from.forEach(halfDamageFrom => console.log("half damage from " + halfDamageFrom.name))
-    
-            typejsonData.damage_relations.no_damage_from.forEach(noDamageFrom => console.log("no damage from " + noDamageFrom.name))
-    
-            typejsonData.damage_relations.no_damage_to.forEach(noDamageTo => console.log("no damage to " + noDamageTo.name))
+const pokemonNames = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
+
+// for (let i = 0; i < pokemonNames.length; i++) {
+//     let li = document.createElement('li');
+//     li.innerHTML = `<option value='${pokemonNames[i]}'`;
+//     pokemonInput.append('li');
+// }
+
+
+
+
+
+
+
+let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/'
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let response = input.value.toLowerCase();
+    fetch(pokeUrl + response)
+    .then((responseData) => {
+        return responseData.json();
+    })
+    .then((jsonData) => {
+        jsonData.types.forEach((result) => {
+            fetch(result.type.url)
+            .then((typeresonseData) => {
+                return typeresonseData.json();
+            })
+            .then((typejsonData) => {
+                typejsonData.damage_relations.double_damage_from.forEach(doubleDamageFrom => console.log("double damage from " + doubleDamageFrom.name))
+        
+                typejsonData.damage_relations.double_damage_to.forEach(doubleDamageTo => console.log("double damage to " + doubleDamageTo.name))
+        
+                typejsonData.damage_relations.half_damage_from.forEach(halfDamageFrom => console.log("half damage from " + halfDamageFrom.name))
+        
+                typejsonData.damage_relations.no_damage_from.forEach(noDamageFrom => console.log("no damage from " + noDamageFrom.name))
+        
+                typejsonData.damage_relations.no_damage_to.forEach(noDamageTo => console.log("no damage to " + noDamageTo.name))
+            })
         })
     })
 })
