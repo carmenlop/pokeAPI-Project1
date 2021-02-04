9110aa5a6ece316bac13b4c136dcae5209a25742
carmenlop <cllopez92@gmail.com>
2021-02-04 13:47:01 -0800

    debugging and css edits

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ console.log('sanity check!')
 // https://pokeapi.co/api/v2/pokemon/{name}
 
 
-let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/'
+let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -18,7 +18,10 @@ form.addEventListener('submit', (e)=>{
         document.getElementById('pokeImageFront').setAttribute('src', jsonData.sprites.front_default);
         document.querySelector('.pokeName').setAttribute('id', 'pokeName');
         jsonData.types.forEach((result) => {
-            document.getElementById('pokeType').textContent = result.type.name;
+            let pokeType = document.createElement('li');
+            pokeType.setAttribute('class', 'type');
+            pokeType.textContent = result.type.name;
+            document.getElementById('pokeType').append(pokeType);
             fetch(result.type.url)
             .then((typeresonseData) => {
                 return typeresonseData.json();
@@ -26,39 +29,39 @@ form.addEventListener('submit', (e)=>{
             .then((typejsonData) => {
                 
                 if (typejsonData.damage_relations.double_damage_to.length > 0) {
-                    document.getElementById('doubleDamageToList').textContent = "Double Damage To"
+                    document.getElementById('doubleDamageToList').textContent = "Double Damage To";
                 } else {
-                    document.getElementById('doubleDamageToList').textContent = "No types receive double damage from your Pokemon"
+                    document.getElementById('doubleDamageToList').textContent = "No types receive double damage from your Pokemon";
                 }
 
                 if (typejsonData.damage_relations.double_damage_from.length > 0) {
-                    document.getElementById('doubleDamageFromList').textContent = "Double Damage From"
+                    document.getElementById('doubleDamageFromList').textContent = "Double Damage From";
                 } else {
-                    document.getElementById('doubleDamageFromList').textContent = "No types give double damage to your Pokemon"
+                    document.getElementById('doubleDamageFromList').textContent = "No types give double damage to your Pokemon";
                 }
 
                 if (typejsonData.damage_relations.half_damage_to.length > 0) {
-                    document.getElementById('halfDamageToList').textContent = "Half Damage To"
+                    document.getElementById('halfDamageToList').textContent = "Half Damage To";
                 } else {
-                    document.getElementById('halfDamageToList').textContent = "No types receive half damage from your Pokemon"
+                    document.getElementById('halfDamageToList').textContent = "No types receive half damage from your Pokemon";
                 }
 
                 if (typejsonData.damage_relations.half_damage_from.length > 0) {
-                    document.getElementById('halfDamageFromList').textContent = "Half Damage From"
+                    document.getElementById('halfDamageFromList').textContent = "Half Damage From";
                 } else {
-                    document.getElementById('halfDamagFromList').textContent = "No types give half damage to your Pokemon"
+                    document.getElementById('halfDamagFromList').textContent = "No types give half damage to your Pokemon";
                 }
 
                 if (typejsonData.damage_relations.no_damage_to.length > 0) {
-                    document.getElementById('noDamageToList').textContent = "No Damage To"
+                    document.getElementById('noDamageToList').textContent = "No Damage To";
                 } else {
-                    document.getElementById('noDamageToList').textContent = "No types receive zero damage from your Pokemon"
+                    document.getElementById('noDamageToList').textContent = "No types receive zero damage from your Pokemon";
                 }
 
                 if (typejsonData.damage_relations.no_damage_from.length > 0) {
-                    document.getElementById('noDamageFromList').textContent = "No Damage From"
+                    document.getElementById('noDamageFromList').textContent = "No Damage From";
                 } else {
-                    document.getElementById('noDamageFromList').textContent = "No types give zero damage to your Pokemon"
+                    document.getElementById('noDamageFromList').textContent = "No types give zero damage to your Pokemon";
                 }
 
                 typejsonData.damage_relations.double_damage_from.forEach(doubleDamageFrom => {
@@ -66,46 +69,44 @@ form.addEventListener('submit', (e)=>{
                     ddfItem.setAttribute('class', 'test');
                     ddfItem.innerHTML = doubleDamageFrom.name;
                     document.getElementById('doubleDamageFromList').append(ddfItem);
-                })
+                });
         
                 typejsonData.damage_relations.double_damage_to.forEach(doubleDamageTo => {
                     let ddtItem = document.createElement('li');
                     ddtItem.setAttribute('class', 'test');
                     ddtItem.innerHTML = doubleDamageTo.name; document.getElementById('doubleDamageToList').append(ddtItem);
-                })
+                });
         
                 typejsonData.damage_relations.half_damage_from.forEach(halfDamageFrom => {
                     let hdfItem = document.createElement('li');
                     hdfItem.setAttribute('class', 'test');
-                    hdfItem.innerHTML = halfDamageFrom.name
-                    document.getElementById('halfDamageFromList').append(hdfItem)
-                })
+                    hdfItem.innerHTML = halfDamageFrom.name;
+                    document.getElementById('halfDamageFromList').append(hdfItem);
+                });
 
                 typejsonData.damage_relations.half_damage_to.forEach(halfDamageTo => {
                     let hdtItem = document.createElement('li');
                     hdtItem.setAttribute('class', 'test');
-                    hdtItem.innerHTML = halfDamageTo.name
-                    document.getElementById('halfDamageToList').append(hdtItem)
-                })
+                    hdtItem.innerHTML = halfDamageTo.name;
+                    document.getElementById('halfDamageToList').append(hdtItem);
+                });
         
                 typejsonData.damage_relations.no_damage_from.forEach(noDamageFrom => {
                     let ndfItem = document.createElement('li');
                     ndfItem.setAttribute('class', 'test');
                     ndfItem.innerHTML = noDamageFrom.name;
                     document.getElementById('noDamageFromList').append(ndfItem);
-                })
+                });
         
                 typejsonData.damage_relations.no_damage_to.forEach(noDamageTo => {
                     let ndtItem = document.createElement('li');
                     ndtItem.setAttribute('class', 'test');
-                    ndtItem.innerHTML = noDamageTo.name
-                    document.getElementById('noDamageToList').append(ndtItem)
-                })
-                })
-                
-            })
-    
-        })
+                    ndtItem.innerHTML = noDamageTo.name;
+                    document.getElementById('noDamageToList').append(ndtItem);
+                });
+                });
+            });
+        });
 
         let formReset = document.getElementById('form');
         formReset.innerHTML = '';
@@ -120,6 +121,6 @@ form.addEventListener('submit', (e)=>{
         buttonElement.addEventListener("click", (evt) => {
         evt.preventDefault();
         location.reload();
-    })
+    });
     
-})
+});
